778085d6c9dac4e3df440b60114000d4f04891bc
Shelton Mutambirwa <sheltonmutambirwa@gmail.com>
2024-11-26 21:37:42 +0200

    Undergraduate data creation completed

--- src/components/home/utilities/data/dist/data.dev.js
+++ src/components/home/utilities/data/dist/data.dev.js
@@ -17,20 +17,21 @@ var api_simulation_data = {
     essay: "Yes",
     about: "Higher Life Foundation provides financial assistance to  fifty top performing high school graduates. 70% of the scholarships are allocated for Science, Technology, Engineering & Mathematics (STEM) studies, whilst 30% is reserved for other disciplines.",
     eligibility: ["A minimum of 15 and 12 points at A level for boys and girls respectively", "Under 21 years of age"],
-    requirements: ["Copies of examination results", "Birth Certificate", "National ID", "Character reference form"],
-    benefits: ["1", "2", "3"]
+    requirements: ["Fill application form", "Submit Copies of examination results", "Submit Birth Certificate", "National ID", "Submit Character reference form"],
+    benefits: ["Leadership Seminars", "Tuition", "Project Support"]
   }, {
     id: "1",
     amount: "All Expenses Covered",
     name: "Old Mutual",
-    opens: "09/23/24",
-    closes: "11/11/03",
+    opens: "30/09/25",
+    closes: "02/10/25",
     merit: "Yes",
-    need: "No",
-    essay: "Yes",
-    about: "Old Mutual provides financial assistance to  fifty top performing high school graduates",
-    requirements: ["1", "2", "3"],
-    benefits: ["1", "2", "3"]
+    need: "Yes",
+    essay: "No",
+    eligibility: ["Under 25 years of age", "Attempting first degree"],
+    about: "Old Mutual provides financial assistance to high perfoming high school graudates aiming to do their undergraduate studies at a local university.",
+    requirements: ["Participate in Old Mutual Math Olympiad", "Submit Examination Results", "Submit Recommendation Letter"],
+    benefits: ["Laptop", "Career Guidance", "Stipend", "Accomodation", "Tuition"]
   }, {
     id: "2",
     amount: "All Expenses Covered",
@@ -38,11 +39,51 @@ var api_simulation_data = {
     opens: "09/23/24",
     closes: "11/11/03",
     merit: "Yes",
-    need: "No",
+    need: "Yes",
     essay: "Yes",
-    about: "Nedbank provides financial assistance to  fifty top performing high school graduates",
-    requirements: ["1", "2", "3"],
-    benefits: ["1", "2", "3"]
+    eligibility: ["15 points in three A level STEM subjects", "Zimbabwean Citizen", "Attempting first degree"],
+    about: "Nedbank provides financial assistance to high achieving A levels aiming to do a STEM undergraduate studies at a local university.",
+    requirements: ["Fill Application Form", "Submit one page application letter", "Submit Headmaster's Motivational Letter", "Submit two recent passport sized photos", "Submit Certified Copies of Birth Certificate and National ID", "Submit Exam Certificates"],
+    benefits: ["Tuition", "Accomodation"]
+  }, {
+    id: "3",
+    amount: "All Expenses Covered",
+    name: "NOIC",
+    opens: "15/02/25",
+    closes: "25/04/25",
+    merit: "Yes",
+    need: "Yes",
+    essay: "Yes",
+    eligibility: ["13+ points in three A level subjects", "21 years of age and under", "Going into first semester"],
+    about: "NOIC provides financial assistance to high achieving A levels aiming to do specified majors within commercials and sciences at a local university.",
+    requirements: ["Fill application form", "Submit university admission letter", "Submit Certified Copies of Academic Certficates, Birth Certificate and National ID", "Submit Recommendation Letter", "Submit 500 word motivational letter"],
+    benefits: ["Tuition", "Accomodation", "Internship"]
+  }, {
+    id: "4",
+    amount: "All Expenses Covered",
+    name: "POTRAZ",
+    opens: "10/05/25",
+    closes: "30/06/25",
+    merit: "Yes",
+    need: "Yes",
+    essay: "Yes",
+    eligibility: ["15 points in three A level STEM subjects", "Zimbabwean Citizen", "Have unconditional offer letter"],
+    about: "POTRAZ provides financial assistance to high achieving A levels aiming to do a STEM undergraduate studies at a local university.",
+    requirements: ["Fill Application Form", "Submit admissin letter", "Submit Recommendation Letter", "Submit Certified Copies of Birth Certificate and National ID", "Submit Exam Certificates"],
+    benefits: ["Tuition", "Accomodation", "Entrepreneurship Training", "Career Guidance"]
+  }, {
+    id: "5",
+    amount: "All Expenses Covered",
+    name: "Mastercard-USIU",
+    opens: "10/15/25",
+    closes: "30/11/25",
+    merit: "Yes",
+    need: "Yes",
+    essay: "Yes",
+    eligibility: ["Under 29 years of age", "Demonstrate academic talent, leadership potential and commitment to giving back", ""],
+    about: "Mastercard provides full financial support to under pivildged high achieving Advanced Level graduates to pursue their first undergraduate studies at USIU in Nairobi,Kenya",
+    requirements: ["Fill Application Form", "Submit one page application letter", "Submit Headmaster's Motivational Letter", "Submit a recent passport photo", "Submit Certified Copies of Birth Certificate and National ID", "Submit Exam Certificates"],
+    benefits: ["Stipend", "Laptop", "Accomodation", "Medical Insurance", "Leadership Training"]
   }],
   "postgraduate": [{
     id: "0",
