0adf1f2e4096b7639e653d088a9ad6fa30c90100
joekawara <joealexanderkawara@gmail.com>
2024-11-28 00:47:54 +0200

    Some descriptions css styling

--- src/components/home/utilities/data/checking/check_component.js
+++ src/components/home/utilities/data/checking/check_component.js
@@ -5,12 +5,11 @@ const CheckItem = (props) => {
 
 
     return (
+        
     <div className="Todo-item">
-
-            <input checked={props.check} onChange={()=>props.handleChange(props.id)} type="checkbox"/>  
-            <span>{props.text}</span>
-
-        </div>)
+        <input checked={props.check} onChange={()=>props.handleChange(props.id)} type="checkbox"/>  
+        <span>{props.text}</span>
+    </div>)
 
     
 }
--- src/components/home/utilities/internships/specific.js
+++ src/components/home/utilities/internships/specific.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import { useSearchParams,useNavigate } from "react-router-dom";
 import { api_simulation_data } from "../data/data";
 
@@ -12,29 +12,50 @@ const Specific = () =>{
     const name = Number(searchParams.get('name'))
     specific_data = relevant_data[name]
 
-    
-    
+    //The puncline is at the bottom just wait and you will laugh
+    const [isHovered, setIsHovered] = useState(false);
+    const handleMouseEnter = () => setIsHovered(true);
+    const handleMouseLeave = () => setIsHovered(false);
+  
+    const style = {
+      backgroundColor: isHovered ? "beige" : "#F5F7FC",
+      border: isHovered ? "2px solid black" : "2px solid blue",
+      cursor: "pointer",
+      margin: "10px",
+      borderRadius:"20px", 
+      padding:"2px 10px"
+    };
 
     //The componnt for adding specific data of the scholarships
     return (
         
-
-        <div style={{display:"flex", justifyContent:"center", alignItems:"center"}}>
-        <div style={{marginTop:"200px", width:"80%", height:"100px", border:"3px, solid"}}>
-            <h1>{specific_data.name}</h1>
+<div style={{display:"flex", justifyContent:"center", alignItems:"center"}}>
+        <div style={{padding: "30px", marginTop:"100px", width:"80%", backgroundColor: "#F5F7FC"}}>
+            <h1 style ={{marginTop: "10px", fontSize: "2em"}}>{specific_data.name}</h1>
             <hr/>
+            <h1 style={{fontSize:"30px", color:"blue"}}>{specific_data.amount}</h1>
             <p>Opens: {specific_data.opens}</p>
             <p>Closes: {specific_data.closes}</p>
             <br/>
-            <h1>Eligibility:</h1>
-            <hr/>
-            <ol>{specific_data.eligibility.map(item => (<li>{item}</li>))}</ol>
+            <h1 style={{color:"blue"}}>About:</h1>
+            <p>{specific_data.about}</p>
             <br/>
-            <h1>Requirements:</h1>
+            <h1 style={{color:"blue"}}>Eligibility:</h1>
             <hr/>
-            <ol>{specific_data.requirements.map(item => (<li>{item}</li>))}</ol>
+            <ol>{specific_data.eligibility.map(item => (<li>🔑{item}</li>))}</ol>
+            <hr/>
+            <br/>
+            <h1 style={{color:"blue"}}>Requirements:</h1>
+            <hr/>
+            <ol>{specific_data.requirements.map(item => (<li>🎓{item}</li>))}</ol>
+            <hr/>
+            <br/>
+            <p>🏆Merit: {specific_data.merit}</p>
+            <p>🚻Need: {specific_data.need}</p>
+            <p>📕Essay: {specific_data.essay}</p>
             <br></br>
-            <button onClick={()=> navigate("/Checklist?name=" + name + "&listName=" + listName)}>Application Checklist</button>
+            <button onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}style ={style}onClick={()=> navigate("/Checklist?name=" + name + "&listName=" + listName)}>Application Checklist</button>
         </div>
    
 </div>
