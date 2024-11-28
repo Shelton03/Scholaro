37198e19dcc98ca80cbdce7cb6fd9c13449d58d3
joekawara <joealexanderkawara@gmail.com>
2024-11-28 13:15:56 +0200

    Changed the buttons to populate different results for the aprrenticeship and scholarships

--- src/components/home/utilities/internships/listing_Component.js
+++ src/components/home/utilities/internships/listing_Component.js
@@ -26,12 +26,24 @@ const ListComponent = (props) =>{
                         <hr style={{marginTop: "20px", borderTop: "1.5px dotted"}}/>
 
                         <div style={{marginTop: "20px"}}>
-                                <p>🐱‍👤Merit Based: {props.merit}</p>
+                                <p>🏆Merit Based: {props.merit}</p>
                                 <p>🚻Need Based: {props.need}</p>
                                 <p>📕Essay Required: {props.essay}</p>
                         </div>
 
-            </div> : <div>no</div>}
+            </div> :  <div className="populating-button"
+                        onClick={() => {navigate("/Specific?name=" + props.id +"&listName=" + props.listName)}}>
+                        <h1 style = {{fontSize: "2em", marginTop: "0px", color: "blackl"}}>{props.name}</h1>
+                        <hr style={{borderTop: "1.5px dotted"}}/>
+                        <p className="deadline-paragraph">opens: {props.opens}</p><p>Closes: {props.closes}</p>
+                        <hr style={{marginTop: "20px", borderTop: "1.5px dotted"}}/>
+
+                        <div style={{marginTop: "20px"}}>
+                                <p>🏆Merit Based: {props.merit}</p>
+                                <p>🚻Need Based: {props.need}</p>
+                        </div>
+
+            </div> }
         </div>
         
     )
