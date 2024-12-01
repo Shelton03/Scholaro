14c2b547a90c36f48d1051cdfe9e4303080b60c5
Chido <chidoshumiraimagumise@gmail.com>
2024-12-01 17:27:16 +0200

    background image styling

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -7,7 +7,7 @@ const Home = () => {
             const navigate = useNavigate()
             
             const backgroundStyle = {
-                backgroundImage: "url('https://wallpapers.com/images/hd/graduation-rites-silhouette-dc6gdzu7y2zb57j9.jpg')",
+                backgroundImage: "url('https://plus.unsplash.com/premium_photo-1683749808307-e5597ac69f1e?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')",
                 backgroundSize: "cover",
                 backgroundPosition: "center",
                 height: "100vh",
